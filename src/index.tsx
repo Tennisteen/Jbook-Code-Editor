06d079e3bd7d1464bf555e43efc33fac68b1fd28
Prakhar <prakharchouhan125@gmail.com>
2024-09-14 16:32:09 +0530

    set up input

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,28 @@
+import { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 
 const App =()=>{
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+
+  const updateInput =(e : React.ChangeEvent<HTMLTextAreaElement>)=>{
+    setInput(e.target.value);
+  }
+
+  const onClick =()=>{
+    
+  }
+
   return (
     <div>
-      Hi
+      <textarea
+       value={input}
+       onChange={updateInput}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
     </div>
   )
 }
