c0f6206eeb8ddd472052cabec22a2d8342eaa869
Prakhar <prakharchouhan125@gmail.com>
2024-09-14 18:33:58 +0530

    transform complete

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,42 @@
-import { useState } from 'react';
+import * as esbuild from 'esbuild-wasm'
+// compiled Go code to do trnaspile and bunddler work 
+import { useEffect, useState, useRef } from 'react';
 import ReactDOM from 'react-dom/client';
 
 const App =()=>{
+  const ref = useRef<any>();
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
+  const startService = async ()=>{
+    ref.current = await esbuild.startService({
+      worker : true,
+      wasmURL : '/esbuild.wasm'
+    });
+    // getting all the compiled Go code in service obj ,so that service obj can be used in transpile and bunddler funtion
+    // console.log(service) =>{build: ƒ, serve: ƒ, transform: ƒ, stop: ƒ}
+    //  build for -> bunddling , transform for -> transpilling
+  }
+  
+  useEffect(()=>{
+    startService();
+  },[]);
+
   const updateInput =(e : React.ChangeEvent<HTMLTextAreaElement>)=>{
     setInput(e.target.value);
-  }
+  };
 
-  const onClick =()=>{
+  const onClick = async()=>{
+    if(!ref.current)
+       return;
     
+    // console.log(ref.current)
+    const result = await ref.current.transform(input,{
+        loader :'jsx',
+        target : 'es2015',
+    });
+    //  console.log(result);
+     setCode(result.code);
   }
 
   return (
